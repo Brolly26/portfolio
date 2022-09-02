4f1fa4d25b8bae298c3559b966eec398d61fb4b8
Biel 26 <88597147+Brolly26@users.noreply.github.com>
2022-09-01 21:41:58 -0300

    Sessão curriculo

--- static/script.js
+++ static/script.js
@@ -9,3 +9,19 @@ menuMobile.classList.contains("bi-list")
 : menuMobile.classList.replace("bi-x", "bi-list");
 body.classList.toggle("menu-nav-active")
 });
+
+/*  Fechar e trocar o menu quando clicar em algum icone */
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item =>{
+item.addEventListener("click",() =>{
+if(body.classList.contains("menu-nav-active")) {
+body.classList.remove("menu-nav-active")
+menuMobile.classList.replace("bi-x" , "bi-list");
+
+}
+    
+})
+
+})
